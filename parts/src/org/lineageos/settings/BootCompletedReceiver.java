49356ddada1c6ca3f63a93645ad9d9742ad188af
Arian <arian.kulmer@web.de>
2020-03-20 23:42:25 +0100

    sweet: parts: Import from xiaomi_sdm845-common
    https://github.com/LineageOS/android_device_xiaomi_sdm845-common/tree/e261035d91b34cf0fdf0f012927e06a2bcdb03ed/parts
    
    Change-Id: I442f48780ca020f3e420ee4ccaf8ac07b05457cb

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -22,20 +22,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-import org.lineageos.settings.haptic.HapticUtils;
-import org.lineageos.settings.thermal.ThermalUtils;
-import org.lineageos.settings.refreshrate.RefreshUtils;
+import org.lineageos.settings.doze.DozeUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
-
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (DEBUG) Log.d(TAG, "Received boot completed intent");
-        HapticUtils.restoreLevel(context);
-        ThermalUtils.initialize(context);
-        RefreshUtils.initialize(context);
+        if (DEBUG)
+            Log.d(TAG, "Received boot completed intent");
+        DozeUtils.onBootCompleted(context);
     }
 }
