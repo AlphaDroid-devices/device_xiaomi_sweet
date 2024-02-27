f84efa5b96f424c4b6d8711653418662554aaae1
UnmoveD <unmoved2121@gmail.com>
2024-02-27 23:06:11 +0100

    sweet: Welcome AlphaDroid

--- lineage_sweet.mk
+++ lineage_sweet.mk
@@ -11,8 +11,31 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common AlphaDroid stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device config
+TARGET_HAS_UDFPS := false
+TARGET_ENABLE_BLUR := false
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# GAPPS
+WITH_GMS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+
+TARGET_INCLUDE_PIXEL_LAUNCHER := false
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+
+# Debugging
+TARGET_INCLUDE_MATLOG := true
+
+# Maintainer
+ALPHA_BUILD_TYPE := Official
+ALPHA_MAINTAINER := UnmoveD
 
 PRODUCT_NAME := lineage_sweet
 PRODUCT_DEVICE := sweet
